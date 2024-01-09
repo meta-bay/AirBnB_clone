9126aa1e87d13be0edf20bc555b7fceef2002bde
meta-bay <bayiemeseret@gmail.com>
2024-01-09 15:02:19 +0300

    pycodestyle compliant

--- models/base_model.py
+++ models/base_model.py
@@ -12,10 +12,11 @@ class BaseModel():
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-    
+
     def __str__(self):
-        return "[{}] ({}) {}".format(__class__.__name__, self.id, self.__dict__)
-    
+        return "[{}] ({}) {}".format(
+            __class__.__name__, self.id, self.__dict__)
+
     def save(self):
         ''' saves the time '''
         self.updated_at = datetime.now()
