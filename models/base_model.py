8eb25368962aa92b307026654bb4c1de5d86a0fe
meta-bay <bayiemeseret@gmail.com>
2024-01-13 20:01:20 +0300

    nothing

--- models/base_model.py
+++ models/base_model.py
@@ -18,9 +18,7 @@ class BaseModel():
                     if key in ['created_at', 'updated_at']:
                         value = datetime.strptime
                         (value, '%Y-%m-%dT%H:%M:%S.%f')
-                        setattr(self, key, value)
-                    else:
-                        setattr(self, key, value)
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
