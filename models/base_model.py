cc628b577330f772da58103fc02b36c22c770dfa
meta-bay <bayiemeseret@gmail.com>
2024-01-13 20:07:53 +0300

    nothing

--- models/base_model.py
+++ models/base_model.py
@@ -12,13 +12,13 @@ class BaseModel():
 
     def __init__(self, *args, **kwargs):
         ''' initializes the object '''
-        if kwargs:
-            for key, value in kwargs.items():
+        if kwargs is not None and kwargs != {}:
+            for key, val in kwargs.items():
                 if key != '__class__':
-                    if key in ['created_at', 'updated_at']:
-                        value = datetime.strptime
-                        (value, '%Y-%m-%dT%H:%M:%S.%f')
-                    setattr(self, key, value)
+                    if key in ('created_at', 'updated_at'):
+                        setattr(self, key, datetime.fromisoformat(val))
+                    else:
+                        setattr(self, key, val)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
