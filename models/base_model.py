3a14abe72d752c8c03bc66808f7a2b2e6df48d74
meta-bay <bayiemeseret@gmail.com>
2024-01-13 21:39:15 +0300

    nothing

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@
 '''
 import uuid
 from datetime import datetime
-
+from models import storage
 
 
 class BaseModel():
@@ -23,6 +23,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         ''' string representation of the object '''
@@ -32,12 +33,12 @@ class BaseModel():
     def save(self):
         ''' saves the time '''
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         ''' to dictionary '''
         the_dict = self.__dict__.copy()
         the_dict["__class__"] = self.__class__.__name__
-        the_dict["id"] = self.id
         the_dict["created_at"] = self.created_at.isoformat()
         the_dict["updated_at"] = self.updated_at.isoformat()
         return the_dict
---
+++ models/engine/file_storage.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+'''
+    FileStorage module
+'''
+import json
+import os
+
+
+class FileStorage():
+    ''' File storage class '''
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        ''' returns the dictionary __objects '''
+        return self.__objects
+
+    def new(self, obj):
+        ''' creates object dictionary '''
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        the_objs = {key: obj}
+        self.__objects.update(the_objs)
+
+    def save(self):
+        ''' to json '''
+        with open(self.__file_path, "w") as file:
+            obj_dict = {key: val.to_dict() for key, val in self.__objects.items()}
+            json.dump(obj_dict, file)
+
+    def reload(self):
+        '''
+            deserializes the JSON file to __objects
+        '''
+        if not os.path.isfile(FileStorage.__file_path):
+            return
+        with open(self.__file_path, 'r', encoding="utf-8") as file:
+            obj_dict = json.load(file)
+            FileStorage.__objects = {key: self.extract_class
+            (val["__class__"])(**val) for key, val in obj_dict.items()
+            }
+
+    def extract_class(self, name=None):
+        ''' extracts the class from the dictionary'''
+        from models.base_model import BaseModel
+
+        if not name:
+            return
+        classes = {
+            "BaseModel": BaseModel
+        }
+        try:
+            return classes[name]
+        except Exception:
+            pass
