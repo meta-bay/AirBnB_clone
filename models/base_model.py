7c5272f0f03316bd84575c07f12247c677f01788
meta-bay <bayiemeseret@gmail.com>
2024-01-13 20:16:16 +0300

    yup

--- models/base_model.py
+++ models/base_model.py
@@ -1,43 +1,43 @@
 #!/usr/bin/python3
-"""
-    BaseMode module
-"""
-
-
-from uuid import uuid4
+'''
+    base model module
+'''
+import uuid
 from datetime import datetime
 
 
 
-class BaseModel:
-    """ Base Model Class"""
+class BaseModel():
+    ''' Base model class '''
 
     def __init__(self, *args, **kwargs):
-        """ initializes the object"""
-        if kwargs is not None and kwargs != {}:
-            for key, val in kwargs.items():
+        ''' initializes the object '''
+        if kwargs:
+            for key, value in kwargs.items():
                 if key != '__class__':
-                    if key in ('created_at', 'updated_at'):
-                        setattr(self, key, datetime.fromisoformat(val))
-                    else:
-                        setattr(self, key, val)
+                    if key in ['created_at', 'updated_at']:
+                        value = datetime.strptime
+                        (value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, value)
         else:
-            self.id = str(uuid4())
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.now()
 
     def __str__(self):
-        """string representation of the object """
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        ''' string representation of the object '''
+        return "[{}] ({}) {}".format(
+            __class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Save Time"""
+        ''' saves the time '''
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """ the dictionary """
+        ''' to dictionary '''
         the_dict = self.__dict__.copy()
-        the_dict['__class__'] = self.__class__.__name__
-        the_dict['created_at'] = the_dict['created_at'].isoformat()
-        the_dict['updated_at'] = the_dict['updated_at'].isoformat()
+        the_dict["__class__"] = self.__class__.__name__
+        the_dict["id"] = self.id
+        the_dict["created_at"] = self.created_at.isoformat()
+        the_dict["updated_at"] = self.updated_at.isoformat()
         return the_dict
