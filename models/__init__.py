6f4e61af4774dfd50ce7f401060cba22e315e463
meta-bay <bayiemeseret@gmail.com>
2024-01-13 18:17:11 +0300

    hope it works

--- models/__init__.py
+++
@@ -1,6 +0,0 @@
-''' The init '''
-from models.engine.file_storage import FileStorage
-
-
-storage = FileStorage()
-storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-''' 
+'''
     base model module
 '''
 
 
 import uuid
-from models import storage
 from datetime import datetime
 
 
@@ -25,7 +24,6 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
 
     def __str__(self):
         ''' string representation of the object '''
@@ -35,7 +33,6 @@ class BaseModel():
     def save(self):
         ''' saves the time '''
         self.updated_at = datetime.now()
-        storage.save()
 
     def to_dict(self):
         ''' to dictionary '''
