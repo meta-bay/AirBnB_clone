e5b7c93f982da7f8cc94ad640b2c7074bcb92f98
meta-bay <bayiemeseret@gmail.com>
2024-01-15 06:45:30 +0300

    work

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -13,19 +13,19 @@ class TestAmenity(TestCase):
     """
 
     def test_pep(self):
-        """test pep"""
-        style = pycodestyle.StyleGuide(quiet=True)
-        result = style.check_files(['models/amenity.py',
-                                    'tests/test_models/test_amenity.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+        """Test PEP 8 compliance"""
+        style_checker = pycodestyle.StyleGuide(quiet=True)
+        result = style_checker.check_files(
+            ['models/amenity.py', 'tests/test_models/test_amenity.py'])
+        self.assertEqual(
+            result.total_errors, 0, "Found code style errors (and warnings).")
 
     def test_module_doc(self):
-        """test module documentation"""
-        doc = __import__('models.amenity').__doc__
-        self.assertGreater(len(doc), 1)
+        """Test module documentation"""
+        module_doc = __import__('models.amenity').__doc__
+        self.assertGreater(len(module_doc), 1)
 
     def test_class_doc(self):
-        """test class documentation"""
-        doc = Amenity.__doc__
-        self.assertGreater(len(doc), 1)
+        """Test class documentation"""
+        class_doc = Amenity.__doc__
+        self.assertGreater(len(class_doc), 1)
